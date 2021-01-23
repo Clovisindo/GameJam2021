a95bb5ea9eb63a1c14b0ac9497a6d3c9e2b99653
Damian Mendez <kiliranshe@hotmail.com>
2021-01-23 20:58:15 +0100

    Cambios en la UI

---
+++ GamJamJan2021/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public int health;
+    public int attack;
+
+    public int numOfHearts;
+    public int numOfAttackPower;
+    public static HealthManager instance = null;
+    public Canvas UICanvas;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+    public Sprite emptyHeart;
+
+    public Image[] attacks;
+    public Sprite fullAttack;
+    public Sprite emptyAttack;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+     
+        Init();
+    }
+
+    private void Init()
+    {
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            // este if define cuando el corazon carga la imagen rellena o no, segun la cantidad de vida
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            // controla el numero de corazones maximos que se muestran( los previamente creados en los objetos)
+            //mejora para esto seria instanciarlo desde el iniciador
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        //health
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            // este if define cuando el corazon carga la imagen rellena o no, segun la cantidad de vida
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            // controla el numero de corazones maximos que se muestran( los previamente creados en los objetos)
+            //mejora para esto seria instanciarlo desde el iniciador
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+        //attack
+        if (attack > numOfAttackPower)
+        {
+            attack = numOfAttackPower;
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            // este if define cuando el corazon carga la imagen rellena o no, segun la cantidad de vida
+            if (i < attack)
+            {
+                attacks[i].sprite = fullAttack;
+            }
+            else
+            {
+                attacks[i].sprite = emptyAttack;
+            }
+
+            // controla el numero de corazones maximos que se muestran( los previamente creados en los objetos)
+            //mejora para esto seria instanciarlo desde el iniciador
+            if (i < numOfAttackPower)
+            {
+                attacks[i].enabled = true;
+            }
+            else
+            {
+                attacks[i].enabled = false;
+            }
+        }
+    }
+
+    public void UpdateUIHealth(int _health)
+    {
+        health = _health;
+    }
+    public void UpdateUIAttack(int _attack)
+    {
+        attack = _attack;
+    }
+}
---
+++ GamJamJan2021/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int playerHealth = 3;
+    public int playerAttack = 1; // con esto mediremos la dificultad dinamicamente
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Aplicar daño al jugador
+    /// </summary>
+    /// <param name="damage"> pasar el numero en valor absoluto</param>
+    public void TakeDamage(int damage)
+    {
+        playerHealth -= damage;
+        UpdatePlayerHealth();
+    }
+    /// <summary>
+    /// Curar al jugador
+    /// </summary>
+    /// <param name="heal"></param>
+    public void HealthDamage(int heal)
+    {
+        playerHealth += heal;
+        UpdatePlayerHealth();
+    }
+
+    public void attackUp(int attUp)
+    {
+        playerAttack += attUp;
+
+    }
+
+    public void UpdatePlayerHealth()
+    {
+        HealthManager.instance.UpdateUIHealth(playerHealth);
+    }
+
+    public void UpdatePlayerAttack()
+    {
+        HealthManager.instance.UpdateUIAttack(playerHealth);
+    }
+}
