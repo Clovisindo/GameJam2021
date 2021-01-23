8b9181bea8658e26c4db01036f1460e24af1e41e
Damian Mendez <kiliranshe@hotmail.com>
2021-01-23 17:35:29 +0100

    Primer prototipo funcional

--- GamJamJan2021/Assets/Scripts/CardScript.cs
+++ GamJamJan2021/Assets/Scripts/CardScript.cs
@@ -1,18 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardScript : MonoBehaviour
 {
+    public static bool DO_NOT = false;// flag
+
+    [SerializeField]
+    private int _state;//estado
+    [SerializeField]
+    private int _cardValue;//valor
+    [SerializeField]
+    private bool _initialized = false;
+
+    private Sprite _cardBack;//dorso carta
+    private Sprite _cardFace;//carta
+
+    private GameObject _manager;//manager
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _state = 1;
+        _manager = GameObject.FindGameObjectWithTag("Manager");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setupGraphics()// inicializa cada carta desde el GameManager( se hace cada turno??)
     {
-        
+        _cardBack = _manager.GetComponent<GameManager>().getCardBack();
+        _cardFace = _manager.GetComponent<GameManager>().getCardFace(_cardValue);
+
+        flipcard();
+    }
+
+    public void flipcard()//segun el estado definido, se renueva el estado de la carta en el tablero
+    {
+        if (_state == 0)
+            _state = 1;
+        else if (_state == 1)
+            _state = 0;
+
+        if (_state == 0 && !DO_NOT)
+            GetComponent<Image>().sprite = _cardBack;
+        else if (_state == 1 && !DO_NOT)
+            GetComponent<Image>().sprite = _cardFace;
+    }
+    public int cardValue
+    {
+        get { return _cardValue; }
+        set { _cardValue = value; }
+    }
+
+    public int state
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    public bool initialized
+    {
+        get { return _initialized; }
+        set { _initialized = value; }
+    }
+
+    public void falseCheck()//rutina de vuelta a la carta
+    {
+        StartCoroutine(pause());
+    }
+        IEnumerator pause()
+    {
+        yield return new WaitForSeconds(0.2F);
+        if (_state == 0)
+            GetComponent<Image>().sprite = _cardBack;
+        else if (_state == 1)
+            GetComponent<Image>().sprite = _cardFace;
+        DO_NOT = false;
     }
 }
---
+++ GamJamJan2021/Assets/Scripts/timeScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timeScript : MonoBehaviour
+{
+    public Text counterText;
+    public bool timeCounter = true;
+    public float seconds, minutes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (timeCounter)
+		{
+			seconds = (int)(Time.timeSinceLevelLoad % 60f);
+			counterText.text = "Seconds" + ":" + seconds.ToString("00");
+		}
+	}
+
+	public void endGame()// se llama al acabar la partida 
+	{
+		timeCounter = false;
+		counterText.color = Color.yellow;
+	}
+}
